dcc656c15e898a5fb84eef5be3b50f14ab7390c2
zhaoMengXiao <401593539@qq.com>
2020-09-11 11:28:07 +0800

    解决更新冲突，添加上传冲突

--- TestJava/src/ThreadTest.java
+++ TestJava/src/ThreadTest.java
@@ -8,8 +8,6 @@ public class ThreadTest {
         SaleTicketThread t3 = new SaleTicketThread("窗口3");
         SaleTicketThread t4 = new SaleTicketThread("窗口4");
         SaleTicketThread t5 = new SaleTicketThread("窗口5");
-
-        System.out.print("test");
         t1.start();
         t2.start();
         t3.start();
