c59ff4c2c19749cdb834e20f17b18aaab9a6bdb4
zhaoMengXiao <401593539@qq.com>
2020-09-11 11:24:15 +0800

    添加工程至本地仓库

---
+++ TestJava/src/ThreadTest.java
@@ -0,0 +1,16 @@
+
+public class ThreadTest {
+
+    public static void main(String[] args) {
+
+        SaleTicketThread t1 = new SaleTicketThread("窗口1");
+        SaleTicketThread t2 = new SaleTicketThread("窗口2");
+        SaleTicketThread t3 = new SaleTicketThread("窗口3");
+        SaleTicketThread t4 = new SaleTicketThread("窗口4");
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
